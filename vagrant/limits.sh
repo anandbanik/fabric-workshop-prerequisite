a66cfd61e7bf2a57410631d8803f72840ab5aef5
anandbanik <anandbanik@gmail.com>
2021-06-20 19:08:57 -0500

    updated name references

--- vagrant/limits.sh
+++ vagrant/limits.sh
@@ -2,8 +2,8 @@
 # set custom limits
 # ----------------------------------------------------------------
 
-cat <<EOF >/etc/security/limits.d/99-techbytes.conf
-# custom limits for techbyte sample demo
+cat <<EOF >/etc/security/limits.d/99-fabric.conf
+# custom limits for fabric workshop
 
 *       soft    nofile          10000
 *       hard    nofile          16384
--- vagrant/nodeJs.sh
+++ vagrant/nodeJs.sh
@@ -1,7 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # ----------------------------------------------------------------
-# Install NodeJs 12.x
+# Install NodeJs 16.x
 # ----------------------------------------------------------------
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 apt-get install -y nodejs
